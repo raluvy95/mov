67691f61a327ca19745d5e03fda9c9e66c894378
raluvy95 <ralucadoge069@gmail.com>
2023-05-05 21:44:03 +0300

    revived /jolly command cuz yes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+import { CommandInteraction } from 'eris';
 dotenv.config()
 import { client } from './client/Client';
 import { debug } from './utils/debug';
@@ -17,6 +18,15 @@ client.on("error", (e) => {
 
 client.on("debug", (i) => {
     debug(i)
+})
+
+client.on("interactionCreate", i => {
+    if (i.type == 2) {
+        const data = i as CommandInteraction
+        if (data.data.name.startsWith("jolly")) {
+            i.createMessage("Slash command is not supported, use traditional command instead.")
+        }
+    }
 })
 
 client.connect()
