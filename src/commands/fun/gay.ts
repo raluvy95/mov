38679699d36b3ff459e26cd686bbbd609b76eb31
raluvy95 <ralucadoge069@gmail.com>
2023-09-24 18:04:10 +0300

    added message info for gay :)

--- src/commands/fun/gay.ts
+++ src/commands/fun/gay.ts
@@ -14,13 +14,51 @@ async function generator(msg: Message, args: string[]) {
         }
     }
 
+    const gay_percent = Math.floor(
+        Math.random() * 101,
+    )
+
+    let gay_msg = "Not really gay."
+
+    switch (Math.floor(gay_percent / 10)) {
+        case 1:
+            gay_msg = "Not gay."
+            break
+        case 2:
+            gay_msg = "I can tell you are not really gay."
+            break
+        case 3:
+            gay_msg = "People think you're gay, but you aren't (yet) :)"
+            break
+        case 4:
+            gay_msg = "You used to act gay on purpose."
+            break
+        case 5:
+            gay_msg = "I can tell you act gay unironically, don't you?"
+            break
+        case 6:
+            gay_msg = "You may like men... ironically."
+            break
+        case 7:
+            gay_msg = "Not enough to love other men"
+            break
+        case 8:
+            gay_msg = "Almost there :)"
+            break
+        case 9:
+            gay_msg = "Allmmooossttt theeerreee :D"
+            break
+        case 10:
+            gay_msg = "Real gay"
+            break
+    }
+
     const e = new MovEmbed()
         .setTimestamp(undefined)
         .setDesc(
-            `${author.username} is **${Math.floor(
-                Math.random() * 101,
-            )}%** gay! 🏳️‍🌈`,
+            `${author.username} is **${gay_percent}%** gay! 🏳️‍🌈`,
         )
+        .setFooter(gay_msg)
         .build();
     client.createMessage(msg.channel.id, e);
 }
