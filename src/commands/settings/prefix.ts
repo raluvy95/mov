a70f09d9961348b408cbc91e51d13085a21d472b
raluvy95 <ralucadoge069@gmail.com>
2023-09-26 21:29:14 +0300

    added confirmation on global prefix

--- src/commands/settings/prefix.ts
+++ src/commands/settings/prefix.ts
@@ -1,7 +1,9 @@
-import { Message } from "eris";
+import { Message, TextChannel } from "eris";
 import { client } from "../../client/Client";
 import { MovCommand } from "../../client/Command";
 import { ISettingsDB } from "../../interfaces/database";
+import { makeid } from "../../utils/makeid";
+import { MessageCollector } from "eris-collect";
 
 async function generator(msg: Message, args: string[]) {
     if (args.length < 1) {
@@ -14,14 +16,33 @@ async function generator(msg: Message, args: string[]) {
         );
         return;
     }
-    client.database.settings.set(
-        `${msg.guildID!}.prefix`,
-        args[0].replace(/"/g, ""),
-    );
+
+    const id = makeid(6);
+    const filter = (message: Message) => message.author.id === msg.author.id;
+    const collector = new MessageCollector(client, msg.channel as TextChannel, {
+        filter,
+        time: 30000,
+        max: 1,
+    });
     client.createMessage(
         msg.channel.id,
-        `Sucessfully set global prefix to \`${args[0]}\`!`,
+        `Are you sure you want to change this bot's prefix to \`${args[0].replace(/"/g, "")}\`?\nPlease type \`${id}\` to confirm!`,
     );
+    collector.on("end", (c) => {
+        const msg = c[0];
+        if (msg.content === id) {
+            client.database.settings.set(
+                `${msg.guildID!}.prefix`,
+                args[0].replace(/"/g, ""),
+            );
+            client.createMessage(
+                msg.channel.id,
+                `Sucessfully set global prefix to \`${args[0]}\`!`,
+            );
+        } else {
+            client.createMessage(msg.channel.id, "Ok, cancelled.");
+        }
+    });
 }
 
 class Prefix extends MovCommand {
